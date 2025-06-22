795b209d71a97bd54592088eacd81e97490963f3
Alvin Muira <alvinmuira@gmail.com>
2025-06-22 03:26:39 +0300

    Finish

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(array, theName) {
+  return array.filter(driver => driver.toLowerCase() === theName.toLowerCase());
+}
+function fuzzyMatch(array, theLetter) {
+    return array.filter(driver=> driver.startsWith(theLetter));
+}
+function matchName(array, theName) {
+    return array.filter(driver=> driver.name === theName);
+}
